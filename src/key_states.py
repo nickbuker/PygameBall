0628d5d7f3095f23f4b669eae997baabb6f70a40
Nick Buker <nickbuker@gmail.com>
2019-05-01 20:45:03 -0700

    add key states and splat sound

---
+++ src/key_states.py
@@ -0,0 +1,7 @@
+class KeyStates:
+
+    def __init__(self):
+        self.key_states = {
+            'L': False,
+            'R': False
+        }
--- src/play_ball.py
+++ src/play_ball.py
@@ -1,16 +1,25 @@
 import pygame
 
 from src.ball import Ball
+from src.key_states import KeyStates
 
 
 pygame.init()
 
 
 # constants
-SCREEN_SIZE = (700, 500)
+SCREEN_SIZE = (1500, 600)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BALL_EDGE_WIDTH = 0
+
+
+# soundz
+pygame.mixer.pre_init(22050, -16, 2, 1024)
+pygame.init()
+pygame.mixer.quit()
+pygame.mixer.init(22050, -16, 2, 1024)  # not janky at all :)
+splat_sound = pygame.mixer.Sound('../assets/splat.ogg')
 
 
 def main():
@@ -23,6 +32,7 @@ def main():
         speed_y=0,
         color=WHITE
     )
+    key_states = KeyStates()
     MIN_X = 0 + ball.width
     MAX_X = SCREEN_SIZE[0] - ball.width
     MAX_Y = SCREEN_SIZE[1] - ball.height
@@ -33,17 +43,32 @@ def main():
                 active = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    ball.update_speed_x(speed_x=ball.speed_x + 1)
+                    key_states.key_states['R'] = True
                 if event.key == pygame.K_LEFT:
-                    ball.update_speed_x(speed_x=ball.speed_x - 1)
+                    key_states.key_states['L'] = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    key_states.key_states['R'] = False
+                if event.key == pygame.K_LEFT:
+                    key_states.key_states['L'] = False
+
         screen.fill(BLACK)
-        # TODO add key states
+        if key_states.key_states['R']:
+            ball.update_speed_x(speed_x=ball.speed_x + 1)
+        if key_states.key_states['L']:
+            ball.update_speed_x(speed_x=ball.speed_x - 1)
+        if not key_states.key_states['R'] and ball.speed_x > 0:
+            ball.update_speed_x(speed_x=ball.speed_x - 1)
+        if not key_states.key_states['L'] and ball.speed_x < 0:
+            ball.update_speed_x(speed_x=ball.speed_x + 1)
         if ball.x < MIN_X:
             ball.x = MIN_X
             ball.speed_x *= -1
+            splat_sound.play()
         if ball.x > MAX_X:
             ball.x = MAX_X
             ball.speed_x *= -1
+            splat_sound.play()
         if ball.y > MAX_Y:
             ball.y = MAX_Y
             ball.speed_y *= -1
