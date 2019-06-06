d174b2d440f5835df7335c580c2e050693a0464a
Nick Buker <nickbuker@gmail.com>
2019-06-05 19:47:08 -0700

    add confetti code stub

---
+++ src/confetti.py
@@ -0,0 +1,8 @@
+class Confetti:
+
+    def __init__(self, x, y, color, width, height):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = width
+        self.height = height
